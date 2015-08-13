06b9b3626e7711e14e2a73734670a4b01db1b97a
Jared Allard <jaredallard@outlook.com>
2015-08-13 16:56:24 -0700

    re-add ssl support

--- js/functions.js
+++ js/functions.js
@@ -13,7 +13,7 @@ function getImages(search, page) {
 	console.log("Hash is now: "+hash);
 	console.log("Communicating with the API.")
 
-	$.get("http://192.241.220.134:3000/search/"+search+"/"+page, function(data) {
+	$.get("https://jaredallard.me:3000/search/"+search+"/"+page, function(data) {
 		if (data.success !== true) {
 			$("#final").html("<h2 class='error'>"+data.message+"</h2>");
 			$("#nav").html("<div class='nav-wrapper'><button class='btn btn-default btn-lg btn-block' onclick='resetPage()'>Back</button></div>");
